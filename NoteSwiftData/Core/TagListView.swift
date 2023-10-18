3c227086b6cdae3bac2062dde1cfc2e7de3ab343
brsaca <brsaca@gmail.com>
2023-10-18 11:45:19 -0600

    feat: add tagList view with create/delete see list

---
+++ NoteSwiftData/Core/TagListView.swift
@@ -0,0 +1,64 @@
+//
+//  TagListView.swift
+//  NoteSwiftData
+//
+//  Created by Brenda Saavedra Cantu on 18/10/23.
+//
+
+import SwiftData
+import SwiftUI
+
+struct TagListView: View {
+    // MARK: View Properties
+    @Environment(\.modelContext) private var context
+    @Query(sort: \Tag.name, order: .reverse) var allTags: [Tag]
+    @State var tagText: String = String()
+    
+    var body: some View {
+        List {
+            Section {
+                DisclosureGroup("Create a tag") {
+                    TextField("Enter text", text: $tagText, axis: .vertical)
+                        .lineLimit(2...4)
+                    
+                    Button("Save") {
+                        createTag()
+                    }
+                }
+            }
+            
+            Section {
+                if allTags.isEmpty {
+                    ContentUnavailableView("You don't have any tags yet", systemImage: "tag")
+                } else {
+                    ForEach(allTags) { tag in
+                        VStack(alignment: .leading) {
+                            Text(tag.name)
+                        }
+                    }
+                    .onDelete { indexSet in
+                        indexSet.forEach{ index in
+                            context.delete(allTags[index])
+                        }
+                        try? context.save()
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension TagListView {
+    func createTag() {
+        let tag = Tag(id: UUID().uuidString, name: tagText, notes: [])
+        context.insert(tag)
+        try? context.save()
+        
+        tagText = ""
+    }
+}
+
+// MARK: - Previews
+#Preview {
+    TagListView()
+}
