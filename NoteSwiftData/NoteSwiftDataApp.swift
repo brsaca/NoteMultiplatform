1c0f68ade12200b437af18f8a949a3ed54d5d55b
brsaca <brsaca@gmail.com>
2023-10-18 10:10:20 -0600

    Initial Commit

---
+++ NoteSwiftData/NoteSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteSwiftDataApp.swift
+//  NoteSwiftData
+//
+//  Created by Brenda Saavedra Cantu on 18/10/23.
+//
+
+import SwiftUI
+
+@main
+struct NoteSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
