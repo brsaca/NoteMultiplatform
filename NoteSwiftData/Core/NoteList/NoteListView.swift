244bb234f10a7ffb7c4ca7f82fb048934039d223
brsaca <brsaca@gmail.com>
2023-10-18 13:21:45 -0600

    feat: add Tags in the list and creation

--- NoteSwiftData/Core/NoteList/NoteListView.swift
+++ NoteSwiftData/Core/NoteList/NoteListView.swift
@@ -12,6 +12,7 @@ struct NoteListView: View {
     // MARK: View Properties
     @Environment(\.modelContext) private var context
     @Query(sort: \Note.createdAt, order: .reverse) var allNotes: [Note]
+    @Query(sort: \Tag.name, order: .forward) var allTags: [Tag]
     @State var noteText: String = String()
     
     var body: some View {
@@ -20,6 +21,29 @@ struct NoteListView: View {
                 DisclosureGroup("Create a note") {
                     TextField("Enter text", text: $noteText, axis: .vertical)
                         .lineLimit(2...4)
+                    
+                    DisclosureGroup("Tag With") {
+                        if allTags.isEmpty {
+                            Text("You don't have any tags yet. Please create one from Tags tab")
+                                .foregroundStyle(.gray)
+                        } else {
+                            ForEach(allTags) { tag in
+                                HStack {
+                                    Text(tag.name)
+                                    if tag.isChecked {
+                                        Spacer()
+                                        Image(systemName: "checkmark.circle")
+                                            .symbolRenderingMode(.multicolor)
+                                    }
+                                }
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .contentShape(Rectangle())
+                                .onTapGesture {
+                                    tag.isChecked.toggle()
+                                }
+                            }
+                        }
+                    }
                     
                     Button("Save") {
                         createNote()
@@ -34,6 +58,10 @@ struct NoteListView: View {
                     ForEach(allNotes) { note in
                         VStack(alignment: .leading) {
                             Text(note.content)
+                            if let tags = note.tags,  tags.count > 0 {
+                                Text("Tags:" + tags.map{ $0.name }.joined(separator: ","))
+                                    .font(.caption)
+                            }
                             Text(note.createdAt, style: .time)
                                 .font(.caption)
                         }
@@ -52,7 +80,15 @@ struct NoteListView: View {
 
 extension NoteListView{
     func createNote() {
-        let note = Note(id: UUID().uuidString, content: noteText, tag: nil, createdAt: .now)
+        var tags:[Tag] = [Tag]()
+        allTags.forEach{ tag in
+            if tag.isChecked {
+                tags.append(tag)
+                tag.isChecked = false
+            }
+        }
+        
+        let note = Note(content: noteText, tags: tags)
         context.insert(note)
         try? context.save()
         
